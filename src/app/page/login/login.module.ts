ad1919fa6d3f159c490a3075973e5b3bfc99abd5
Richard.Liao <richard.liao@tpisoftware.com>
2022-10-06 17:35:10 +0800

    chore: 登入頁/註冊頁使用 material 元件

--- src/app/page/login/login.module.ts
+++ src/app/page/login/login.module.ts
@@ -1,5 +1,7 @@
-import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+import {NgModule} from '@angular/core'
+import {MatIconModule} from '@angular/material/icon'
+import {MatInputModule} from '@angular/material/input'
 import {LoginRoutingModule} from './login-routing.module'
 import {LoginComponent} from './login.component'
 
@@ -8,6 +10,6 @@ import {LoginComponent} from './login.component'
  */
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, LoginRoutingModule, MatInputModule, MatIconModule],
 })
 export class LoginModule {}
--- src/app/page/register/register.module.ts
+++ src/app/page/register/register.module.ts
@@ -1,11 +1,13 @@
-import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+import {NgModule} from '@angular/core'
+import {MatIconModule} from '@angular/material/icon'
+import {MatInputModule} from '@angular/material/input'
 
 import {RegisterRoutingModule} from './register-routing.module'
 import {RegisterComponent} from './register.component'
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterRoutingModule],
+  imports: [CommonModule, RegisterRoutingModule, MatInputModule, MatIconModule],
 })
 export class RegisterModule {}
