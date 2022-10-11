c391ff8b8276d57dcb8ac0ad4222787e4682f048
Richard.Liao <richard.liao@tpisoftware.com>
2022-10-11 16:45:59 +0800

    chore: 表格資料型別, angular class 繫結

--- src/app/page/tables/tables.component.ts
+++ src/app/page/tables/tables.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core'
+import {Component} from '@angular/core'
+import {Data} from './tables.interface'
 
+/** 表格頁面 */
 @Component({
   selector: 'app-tables',
   templateUrl: './tables.component.html',
   styleUrls: ['./tables.component.scss'],
 })
 export class TablesComponent {
-  /**資料 */
-  data: any = [
+  /** 表格資料 */
+  data: Array<Data> = [
     {
       author: 'John Michael',
       authorUrl: '../assets/img/team-2.jpg',
@@ -16,7 +18,6 @@ export class TablesComponent {
         role: 'Manager',
         place: 'Organization',
       },
-
       status: 'Online',
       employed: '2023-04-18',
     },
@@ -29,17 +30,17 @@ export class TablesComponent {
         place: 'Organization',
       },
 
-      status: 'Online',
+      status: 'Offline',
       employed: '2023-04-18',
     },
   ]
 
-  constructor() {}
-
   /**
    * 印出迴圈的資料
+   * @param data - API 資料
+   * @returns 無回傳值
    */
-  printConsole(data: any): void {
+  printConsole(data: Data): void {
     console.log(data)
   }
 }
