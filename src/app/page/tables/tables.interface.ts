20299d830f26cb82b2c95295b4ac97085b95aaa6
Richard.Liao <richard.liao@tpisoftware.com>
2022-10-11 16:45:59 +0800

    chore: 表格資料型別, angular class 繫結

---
+++ src/app/page/tables/tables.interface.ts
@@ -0,0 +1,23 @@
+/** 表格資料 */
+export interface Data {
+  /** 作者 */
+  author: string
+  /** 圖片 */
+  authorUrl: string
+  /** 信箱 */
+  email: string
+  /** 功能 */
+  function: Function | null
+  /** 狀態 */
+  status: string
+  /** 雇用日 */
+  employed: string
+}
+
+/** 功能 */
+export interface Function {
+  /**角色 */
+  role: string
+  /**組織 */
+  place: string
+}
