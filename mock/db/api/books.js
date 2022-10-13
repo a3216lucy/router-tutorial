a1a5d27f1c5c9e029a94a4bf3203b7c330f0330a
Richard.Liao <richard.liao@tpisoftware.com>
2022-10-13 17:01:19 +0800

    chore: json server

---
+++ mock/db/api/books.js
@@ -0,0 +1,43 @@
+module.exports = {
+  status: 'A0001',
+  message: 'success',
+  success: true,
+  data: [
+    {
+      title: "TODAY'S MONEY",
+      price: 53000,
+      growth: '+55%',
+      content: 'since yesterday',
+      status: 'money',
+      describe:
+        'since yesterday Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
+    },
+    {
+      title: "TODAY'S USERS",
+      price: 2300,
+      growth: '3%',
+      content: 'since last week',
+      status: 'users',
+      describe:
+        'since last week Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
+    },
+    {
+      title: 'NEW CLIENTS',
+      price: 3462,
+      growth: '-2%',
+      content: 'since last quarter',
+      status: 'client',
+      describe:
+        'since last quarter Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
+    },
+    {
+      title: 'SALES',
+      price: 103430,
+      growth: '5%',
+      content: 'than last month',
+      status: 'sales',
+      describe:
+        'than last month Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
+    },
+  ],
+}
--- src/app/page/index/index.component.ts
+++ src/app/page/index/index.component.ts
@@ -1,3 +1,4 @@
+import {HttpClient} from '@angular/common/http'
 import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core'
 import {GetPopulationService} from 'src/app/shared/services/api/get-population.service'
 
@@ -7,44 +8,7 @@ import {GetPopulationService} from 'src/app/shared/services/api/get-population.s
   styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent implements OnInit, OnDestroy, AfterViewInit {
-  books = [
-    {
-      title: "TODAY'S MONEY",
-      price: 53000,
-      growth: '+55%',
-      content: 'since yesterday',
-      status: 'money',
-      describe:
-        'since yesterday Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
-    },
-    {
-      title: "TODAY'S USERS",
-      price: 2300,
-      growth: '3%',
-      content: 'since last week',
-      status: 'users',
-      describe:
-        'since last week Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
-    },
-    {
-      title: 'NEW CLIENTS',
-      price: 3462,
-      growth: '-2%',
-      content: 'since last quarter',
-      status: 'client',
-      describe:
-        'since last quarter Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
-    },
-    {
-      title: 'SALES',
-      price: 103430,
-      growth: '5%',
-      content: 'than last month',
-      status: 'sales',
-      describe:
-        'than last month Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis id quisquam perferendis enim odit saepe, corrupti repellat voluptates fugiat sapiente sequi sunt nihil iure molestias reiciendis soluta dolore similique voluptatum totam iste fuga doloribus! Cumque tempora, quas id repellendus aut atque vel quod eligendi exercitationem voluptates nesciunt qui iste minus praesentium recusandae ipsam odit soluta expedita, magni, aliquam et veniam?',
-    },
-  ]
+  books = []
 
   cardDetail = {
     title: '',
@@ -62,6 +26,7 @@ export class IndexComponent implements OnInit, OnDestroy, AfterViewInit {
   constructor(
     // DI 注入服務
     private getPopulationService: GetPopulationService,
+    private http: HttpClient,
   ) {
     console.warn('constructor: DOM 尚未載入')
 
@@ -70,11 +35,20 @@ export class IndexComponent implements OnInit, OnDestroy, AfterViewInit {
         this.peopleData = res.result.records
       },
     })
+
+    const requestBody = {status: 'user'}
+    this.http.post('http://localhost:3000/api/books', requestBody).subscribe({
+      next: (res: any) => {
+        this.books = res.data ?? []
+        if (this.books.length !== 0) {
+          this.cardDetail = this.books[0]
+        }
+      },
+    })
   }
 
   ngOnInit() {
     console.warn('ngOnInit: 進入網頁時觸發，DOM 尚未載入')
-    this.cardDetail = this.books[0]
   }
 
   ngAfterViewInit(): void {
