78e6485be7586b2d24d3b62860ae6ae1a27bcfa6
Richard.Liao <richard.liao@tpisoftware.com>
2022-10-17 19:45:41 +0800

    chore: kkbox api 初始值

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core'
+import {Component} from '@angular/core'
 import {SearchService} from '@my-app/core/services/api/kkbox'
 import {NavbarService} from '@my-app/core/services/navbar.service'
 
@@ -7,10 +7,21 @@ import {NavbarService} from '@my-app/core/services/navbar.service'
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   constructor(private searchService: SearchService, private navbarService: NavbarService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    const request = {
+      q: 'Taylor Swift',
+      type: 'track',
+      territory: 'TW',
+    }
+    this.searchService.search(request).subscribe({
+      next: (res) => {
+        this.navbarService.searchData$.next(res.tracks.data)
+      },
+    })
+  }
 
   changeInput($event: Event) {
     const inputValue = ($event.target as HTMLInputElement).value
