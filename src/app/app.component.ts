2ae1fbb9886fcdf741d355e9e2c16bac3712f4eb
Richard.Liao <richard.liao@tpisoftware.com>
2022-10-19 14:48:02 +0800

    chore: rxjs 小教室

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {BehaviorSubject, map} from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app'
+export class AppComponent implements OnInit {
+  //getter 取得資料，不能放參數
+  get title() {
+    return 'Hello'.toUpperCase()
+  }
+
+  // BehaviorSubject + asObservable
+  _arrayA$ = new BehaviorSubject<number[]>([1, 2, 3])
+
+  /**
+   * BehaviorSubject 轉 Observable。
+   * 特性: 只能讀取
+   */
+  arrayA$ = this._arrayA$.asObservable().pipe(
+    map((n) => {
+      return n.map((d) => d * 10)
+    }),
+  )
+
+  ngOnInit() {
+    this._arrayA$.next([4, 5, 6])
+  }
 }
